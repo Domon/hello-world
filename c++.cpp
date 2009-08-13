b843195f78e4795b2d75f8a01a543902ac0b986a
Chun-wei Kuo <Dendoh@gmail.com>
2009-08-14 01:25:14 +0800

    Added php.php and modified c++.cpp, c.c

--- c++.cpp
+++ c++.cpp
@@ -3,4 +3,5 @@ using namespace std;
 
 int main(){
 	cout << "Hello World" << endl;
+	cout << "Hello World2" << endl;
 }
--- c.c
+++ c.c
@@ -2,5 +2,7 @@
 
 int main(void) {
 	printf("Hello World\n");
+	printf("Hello World2\n");
+	printf("Hello World3\n");
 	return 0;
 }
